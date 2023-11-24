59aa80782279d7549d8b0c5b7dd4ea8942952b7d
MaharshiJ88 <136585286+MaharshiJ88@users.noreply.github.com>
2023-11-24 22:59:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	// Use TextEncoder to encode the string to bytes
+    const encoder = new TextEncoder();
+    const bytes = encoder.encode(str);
+
+    // Return the length of the encoded bytes array
+    return bytes.length;
 };
 
 // Do not change the code below
